9ce36cad0fb7dcabc9cc9346c6c63bd7d16e94df
nunch <yiborra@gmail.com>
2015-12-22 13:22:10 +0100

    Signed-off-by: nunch <yiborra@gmail.com>

--- vpproject/generated/Caisse.java
+++ vpproject/generated/Caisse.java
@@ -1,3 +1,14 @@
+import java.util.LinkedList;
+
 public class Caisse {
 	public Cle aCle;
+	public LinkedList<Session> aSession = new LinkedList<Session>();
+
+	public void login(String pSession) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void ouvrirCaisse(String pCle) {
+		throw new UnsupportedOperationException();
+	}
 }
